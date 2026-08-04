47a8588f069aa0c63563da87240205185ac3cdd9
Mebrie Awoke <mebrieawoke941@gmail.com>
2026-08-04 10:28:34 +0300

    Update app.py

--- Backend/app.py
+++ Backend/app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request  
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 from src.helper import download_hugging_face_embeddings
